dd07c2eecd21929483a895166a09801c94e502c1
Arbind007 <almbbsr456@gmail.com>
2020-08-30 16:36:10 +0530

    text-justify

--- src/components/About.js
+++ src/components/About.js
@@ -4,45 +4,50 @@ function About() {
   return (
     <div style={{ backgroundColor: "whitesmoke" }}>
       <div className="container text-center py-5">
-        <h1 className="font-weight-light">
+        <h1 className="text-center font-weight-light">
           <span className="text-info"> About </span> Me
         </h1>
+        <div className="lead">
+          I can develop the front and the back of an app
+        </div>
         <div className="row">
           <div className="col-12 col-md-6 py-3 text-justify">
             <h5 className="text-info pd-3 text-center">What can I do?</h5>
-            All he could think about was how it would all end. There was still a
-            bit of uncertainty in the equation, but the basics were there for
-            anyone to see. No matter how much he tried to see the positive, it
-            wasn't anywhere to be seen. The end was coming and it wasn't going
-            to be pretty.
+            <p className="text-justify">
+              It's been a while, I have been studing more about Web Development
+              and Cyber Security. I sometimes code 14hrs a day. I know basics of
+              many Computer Languages. I also do Grapic Designing as a hobby!
+            </p>
           </div>
           <div className="col-12 col-md-6 py-3 text-justify">
             <h5 className="text-info pd-3 text-center">
               What am I currently doing?
             </h5>
-            All he could think about was how it would all end. There was still a
-            bit of uncertainty in the equation, but the basics were there for
-            anyone to see. No matter how much he tried to see the positive, it
-            wasn't anywhere to be seen. The end was coming and it wasn't going
-            to be pretty.
+            <p className="text-justify">
+              I am prety much learning more about web devlopment. But my main
+              target in my life is to learn about Cyber Security and Ethical
+              Hacking. I have also been learning about Data Structure.To know
+              more about my current progress, please check out my LinkedIn
+              profile!
+            </p>
           </div>
           <div className="col-12 col-md-6 py-3 text-justify">
             <h5 className="text-info pd-3 text-center">
               What do I believe in?
             </h5>
-            All he could think about was how it would all end. There was still a
-            bit of uncertainty in the equation, but the basics were there for
-            anyone to see. No matter how much he tried to see the positive, it
-            wasn't anywhere to be seen. The end was coming and it wasn't going
-            to be pretty.
+            <p className="text-justify">
+              Work Harder! There will be obstacles in your path but they should
+              not stop you from moving forward. Study new things when you really
+              feel like and keep your 100% while doing it.
+            </p>
           </div>
           <div className="col-12 col-md-6 py-3 text-justify">
             <h5 className="text-info pd-3 text-center">How can I help you?</h5>
-            All he could think about was how it would all end. There was still a
-            bit of uncertainty in the equation, but the basics were there for
-            anyone to see. No matter how much he tried to see the positive, it
-            wasn't anywhere to be seen. The end was coming and it wasn't going
-            to be pretty.
+            <p className="text-justify">
+              If you want to code your Web Site, I can do that for you! If you
+              are facing any problem with your code, I am available to help you
+              out.
+            </p>
           </div>
         </div>
       </div>
--- src/components/CertificatePage.js
+++ src/components/CertificatePage.js
@@ -3,7 +3,7 @@ import React from "react";
 function CertificatePage(props) {
   return (
     <div className="py-5 my-5">
-      <h1>Projects {props.match.params.id}</h1>
+      <h1>Certificate {props.match.params.id}</h1>
     </div>
   );
 }
